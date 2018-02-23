f9e91036ecd72b0b4864c62105988a52a710b760
Collin <collin-imac@Collins-iMac.local>
2018-02-23 15:20:43 -0500

    finished first app

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, Button, Image  } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Image, ScrollView, TouchableOpacity, Linking, url  } from 'react-native';
 
 //use npm install --save axios, then import the library to get https requests
 import axios from 'axios';
@@ -36,9 +36,9 @@ class AlbumList extends Component {
     console.log(this.state);
 
     return(
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     );
   }
 }
@@ -47,18 +47,37 @@ class AlbumList extends Component {
 //children elements
 class AlbumDetail extends Component {
   render(){
+    //destructure stylesheet to make code cleaner
+    const { 
+      thumbnailStyle, 
+      headerContentStyle, 
+      thumbnailContainerStyle,
+      headerTextStyle,
+      imageStyle 
+    } = styles;
+
     return(
       <Card>
         <CardSection>
-          <View>
-
+          <View style={thumbnailContainerStyle}>
+            <Image source={{uri: this.props.album.thumbnail_image}} style={thumbnailStyle}/>
           </View>
 
-          <View style={styles.headerContentStyle}>
-            <Text>{this.props.album.title}</Text>
+          <View style={headerContentStyle}>
+            <Text style={headerTextStyle}>{this.props.album.title}</Text>
             <Text>{this.props.album.artist}</Text>
           </View>
 
+        </CardSection>
+
+        <CardSection>
+          <Image source={{ uri: this.props.album.image}} style={imageStyle}/>
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={() => Linking.openURL(this.props.album.url)}>
+            Buy Now
+          </Button>
         </CardSection>
       </Card>
     );
@@ -87,12 +106,26 @@ class CardSection extends Component {
   }
 }
 
+class Button extends Component {
+  render(){
+    const { buttonStyle, buttonText } = styles;
+
+    return(
+      <TouchableOpacity onPress={this.props.onPress} style={buttonStyle}>
+        <Text style={buttonText}>{this.props.children}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
 
 //build the app using components
+//set the root view to flex: 1, this 
+//fills the entire display with the content
 export default class App extends Component {
   render() { 
     return (
-      <View>
+      <View style={{ flex: 1 }}>
         <Header name='Albums'/>
         <AlbumList />
       </View>
@@ -100,7 +133,7 @@ export default class App extends Component {
   }
 }
 
-
+//STYLESHEET//
 const styles = StyleSheet.create({
   viewStyle: {
     backgroundColor : '#F8F8F8',
@@ -143,5 +176,41 @@ const styles = StyleSheet.create({
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  },
+  buttonText: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
   }
 });
